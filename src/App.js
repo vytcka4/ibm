9b297cdb258542962dc601078e10c03e760da497
vytcka4 <vytautas.freelance@gmail.com>
2022-08-14 01:21:32 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [enteredStock, setEnteredStock] = useState([]);
+  let url;
+  const apiKey = "cbrqhjaad3idk2bmtfp0";
+
+  const [companyProfileData, setCompanyProfileData] = useState({});
+  const [fetched, setfetched] = useState(false);
+
+  const GetCompanyData = (event) => {
+    const enteredName = event.target.value;
+    const enteredNameArray = enteredName
+      .split(" ")
+      .filter((Element) => Element.length > 0)
+      .map((el, index) => el[index].trim());
+
+    console.log(enteredNameArray);
+    setEnteredStock(enteredName);
+  };
+  useEffect(() => {
+    const getData = setTimeout(() => {
+      url = `https://finnhub.io/api/v1/stock/profile2?symbol=${enteredStock}&token=${apiKey}`;
+      axios
+        .get(url)
+        .then((response) => {
+          setCompanyProfileData({
+            name: response.data.name,
+            country: response.data.country,
+            currency: response.data.currency,
+            url: response.data.weburl,
+          });
+          console.log(response.data);
+          setfetched(true);
+        })
+        .catch((er) => {});
+    }, 250);
+
+    return () => {
+      clearTimeout(getData);
+    };
+  }, [enteredStock]);
+
+  console.log(companyProfileData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>company name</label>
+        <input minLength="1" type="text" onChange={GetCompanyData}></input>
+        <button> click</button>
+      </form>
+
+      {fetched && (
+        <ul>
+          <li>{companyProfileData.name}</li>
+          <li>{companyProfileData.currency}</li>
+          <li>{companyProfileData.country}</li>
+          <li>{companyProfileData.url}</li>
+        </ul>
+      )}
     </div>
   );
 }
