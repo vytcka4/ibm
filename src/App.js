165fe3b75b6cdbc2eed9c5e9aba322fbd2c711f3
vytcka4 <vytautas.freelance@gmail.com>
2022-08-15 22:27:21 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,103 @@
-import "./App.css";
+import "./App.scss";
 import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [enteredStock, setEnteredStock] = useState([]);
   let url;
   const apiKey = "cbrqhjaad3idk2bmtfp0";
-
+  const [enteredStock, setEnteredStock] = useState([]);
   const [companyProfileData, setCompanyProfileData] = useState({});
   const [fetched, setfetched] = useState(false);
+  const [isLoading, setisLoading] = useState(true);
+  const [suggestions, setSuggestions] = useState([]);
+  const [allStockCodes, setAllStockCodes] = useState([]);
 
-  const GetCompanyData = (event) => {
-    const enteredName = event.target.value;
-    const enteredNameArray = enteredName
-      .split(" ")
-      .filter((Element) => Element.length > 0)
-      .map((el, index) => el[index].trim());
-
-    console.log(enteredNameArray);
-    setEnteredStock(enteredName);
-  };
   useEffect(() => {
-    const getData = setTimeout(() => {
-      url = `https://finnhub.io/api/v1/stock/profile2?symbol=${enteredStock}&token=${apiKey}`;
-      axios
-        .get(url)
-        .then((response) => {
-          setCompanyProfileData({
-            name: response.data.name,
-            country: response.data.country,
-            currency: response.data.currency,
-            url: response.data.weburl,
-          });
-          console.log(response.data);
-          setfetched(true);
-        })
-        .catch((er) => {});
-    }, 250);
-
-    return () => {
-      clearTimeout(getData);
+    const getAllStockCodes = async () => {
+      try {
+        const response = await axios.get(
+          `https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${apiKey}`
+        );
+        setAllStockCodes(response.data);
+        setisLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
     };
-  }, [enteredStock]);
+    getAllStockCodes();
+  }, []);
 
-  console.log(companyProfileData);
+  const onChangeSymbolHandler = (event) => {
+    const enteredName = event.target.value;
+    let matches = [];
+    if (enteredName.length > 0) {
+      matches = allStockCodes.filter((stock) => {
+        const regex = new RegExp(`^${enteredName}`, "gi");
+        return stock.symbol.match(regex);
+      });
+    }
+    setSuggestions(matches);
+    setEnteredStock(enteredName);
+    setfetched(false);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    url = `https://finnhub.io/api/v1/stock/profile2?symbol=${enteredStock}&token=${apiKey}`;
+    axios
+      .get(url)
+      .then((response) => {
+        setCompanyProfileData({
+          name: response.data.name,
+          country: response.data.country,
+          currency: response.data.currency,
+          url: response.data.weburl,
+        });
+        setfetched(true);
+      })
+      .catch((er) => {});
+  };
 
   return (
-    <div className="App">
-      <form>
-        <label>company name</label>
-        <input minLength="1" type="text" onChange={GetCompanyData}></input>
-        <button> click</button>
-      </form>
+    !isLoading && (
+      <div className="App">
+        <form onSubmit={onSubmitHandler}>
+          <div className="form-container">
+            <label for="symbol">company name</label>
+            <input
+              minLength="1"
+              type="text"
+              onChange={onChangeSymbolHandler}
+            ></input>
+            <label for="startDate">Start date: </label>
+            <input type="date"></input>
+            <label for="EndDate">End date: </label>
+            <input type="date"></input>
+            <div className="suggestions-control">
+              {suggestions &&
+                suggestions.map((suggestions, key) => {
+                  return (
+                    <div className="suggestion-container" key={key}>
+                      <p>{suggestions.symbol}</p>
+                      <p>{suggestions.description}</p>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+          <button> click</button>
+        </form>
 
-      {fetched && (
-        <ul>
-          <li>{companyProfileData.name}</li>
-          <li>{companyProfileData.currency}</li>
-          <li>{companyProfileData.country}</li>
-          <li>{companyProfileData.url}</li>
-        </ul>
-      )}
-    </div>
+        {fetched && (
+          <ul>
+            <li>{companyProfileData.name}</li>
+            <li>{companyProfileData.currency}</li>
+            <li>{companyProfileData.country}</li>
+            <li>{companyProfileData.url}</li>
+          </ul>
+        )}
+      </div>
+    )
   );
 }
 
